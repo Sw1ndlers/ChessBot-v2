b569d0d2ffc9b9c4b3539e11784ce459fa739349
Sw1ndlers <lisina2023@icloud.com>
2025-05-10 14:36:15 -0700

    Changes

--- Yolo Training/train.py
+++ Yolo Training/train.py
@@ -1,9 +1,7 @@
 import torch
 from ultralytics import YOLO
 
-torch.cuda.set_device(0) # Comment out this line if you want to use CPU
+torch.cuda.set_device(0) # Verify gpu is available
 
 model = YOLO("../assets/base_yolo11n.pt")
-# model.export(format="onnx")
-
 results = model.train(data="chess.yaml", epochs=150, batch=32, imgsz=800, device=0)
