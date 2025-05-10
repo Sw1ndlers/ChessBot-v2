89103eb05db179583fbd3e03fa7086483476826b
Sw1ndlers <lisina2023@icloud.com>
2025-05-09 19:02:17 -0700

    Update test model output display

--- Yolo Training/test_model.py
+++ Yolo Training/test_model.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import cv2
 
 model = YOLO("best.onnx")  # Just point to your ONNX model
-results = model("test_image.jpg")
+results = model("test_image.png")
 
 # Get annotated image (NumPy array)
 img = results[0].plot(line_width=1, font_size=0.4)  # Smaller boxes + text
