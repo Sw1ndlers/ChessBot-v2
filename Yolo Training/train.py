1c511751a4f9ea29be4869099717d1b45cdb09ab
Sw1ndlers <lisina2023@icloud.com>
2025-05-10 14:40:24 -0700

    Update imgsize

--- Yolo Training/train.py
+++ Yolo Training/train.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 torch.cuda.set_device(0) # Verify gpu is available
 
 model = YOLO("../assets/base_yolo11n.pt")
-results = model.train(data="chess.yaml", epochs=150, batch=32, imgsz=800, device=0)
+results = model.train(data="chess.yaml", epochs=150, batch=32, device=0)
